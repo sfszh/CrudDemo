efa76288eb764fdb42967b4334b91231fdadc72a
Rui Zhang <rayruizhang1@gmail.com>
2021-05-16 20:20:51 +0800

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+import co.ruizhang.buildsrc.Libs
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        applicationId = "co.ruizhang.cruddemo"
+        minSdk = 24
+        targetSdk = 30
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+        useIR = true
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
+        kotlinCompilerVersion = "1.4.32"
+    }
+}
+
+dependencies {
+
+    implementation(Libs.Kotlin.stdlib)
+    implementation(Libs.Coroutines.android)
+
+    implementation(Libs.AndroidX.coreKtx)
+    implementation(Libs.AndroidX.navigation)
+    implementation(Libs.AndroidX.Activity.activityCompose)
+    implementation(Libs.AndroidX.ConstraintLayout.constraintLayoutCompose)
+
+    implementation(Libs.AndroidX.Compose.runtime)
+    implementation(Libs.AndroidX.Compose.foundation)
+    implementation(Libs.AndroidX.Compose.layout)
+    implementation(Libs.AndroidX.Compose.ui)
+    implementation(Libs.AndroidX.Compose.uiUtil)
+    implementation(Libs.AndroidX.Compose.material)
+    implementation(Libs.AndroidX.Compose.animation)
+    implementation(Libs.AndroidX.Compose.iconsExtended)
+    implementation(Libs.AndroidX.Compose.tooling)
+    implementation(Libs.Accompanist.coil)
+    implementation(Libs.Accompanist.insets)
+
+    androidTestImplementation(Libs.AndroidX.Activity.activityCompose)
+    androidTestImplementation(Libs.JUnit.junit)
+    androidTestImplementation(Libs.AndroidX.Test.core)
+    androidTestImplementation(Libs.AndroidX.Test.espressoCore)
+    androidTestImplementation(Libs.AndroidX.Test.rules)
+    androidTestImplementation(Libs.AndroidX.Test.Ext.junit)
+    androidTestImplementation(Libs.AndroidX.Compose.uiTest)
+}
