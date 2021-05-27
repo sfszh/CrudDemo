e834361b5a7067df5a5f5d6863844377ffb3bd2a
Rui Zhang <rayruizhang1@gmail.com>
2021-05-27 12:35:04 +0800

    add simple navigation

--- app/src/main/java/co/ruizhang/cruddemo/NavGraph.kt
+++ app/src/main/java/co/ruizhang/cruddemo/NavGraph.kt
@@ -1,5 +1,17 @@
 package co.ruizhang.cruddemo
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.navigate
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import co.ruizhang.cruddemo.MainDestinations.REPOS_ROUTE
+import co.ruizhang.cruddemo.data.Mock_Repos
+import co.ruizhang.cruddemo.ui.Onboarding
+import co.ruizhang.cruddemo.ui.RepoCard
+import co.ruizhang.cruddemo.ui.theme.CrudDemoTheme
+
 /**
  * Destinations used in the ([OwlApp]).
  */
@@ -8,4 +20,30 @@ object MainDestinations {
     const val REPOS_ROUTE = "repos"
     const val REPO_DETAIL_ROUTE = "repo"
     const val REPO_DETAIL_ID_KEY = "repoId"
+}
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = MainDestinations.ONBOARDING_ROUTE,
+){
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(MainDestinations.ONBOARDING_ROUTE) {
+            Onboarding {
+                navController.navigate(REPOS_ROUTE)
+            }
+        }
+
+        composable(MainDestinations.REPOS_ROUTE) {
+            val mockData = Mock_Repos[0]
+
+            CrudDemoTheme {
+                RepoCard(repo = mockData, onClick = {})
+            }
+        }
+
+    }
 }
--- app/src/main/java/co/ruizhang/cruddemo/ui/MainActivity.kt
+++ app/src/main/java/co/ruizhang/cruddemo/ui/MainActivity.kt
@@ -8,18 +8,14 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import co.ruizhang.cruddemo.NavGraph
 import co.ruizhang.cruddemo.ui.theme.CrudDemoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CrudDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            NavGraph()
         }
     }
 }
