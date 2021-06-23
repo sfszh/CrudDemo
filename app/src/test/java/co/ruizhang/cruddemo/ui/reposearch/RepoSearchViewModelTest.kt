8f150e305aab5133d4c35ee19bc264b3254a4573
Rui Zhang <rayruizhang1@gmail.com>
2021-06-23 18:41:34 +0800

    add unit 1st unit test

--- app/src/test/java/co/ruizhang/cruddemo/ui/reposearch/RepoSearchViewModelTest.kt
+++ app/src/test/java/co/ruizhang/cruddemo/ui/reposearch/RepoSearchViewModelTest.kt
@@ -7,15 +7,15 @@ import co.ruizhang.cruddemo.data.Repository
 import co.ruizhang.cruddemo.data.SearchRepository
 import co.ruizhang.cruddemo.util.MainCoroutineRule
 import co.ruizhang.cruddemo.util.getOrAwaitValue
-import co.ruizhang.cruddemo.util.observeForTesting
-import io.mockk.*
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Rule
-
 import org.junit.Test
 
 class RepoSearchViewModelTest {
@@ -29,8 +29,8 @@ class RepoSearchViewModelTest {
     @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    val searchRepoMock = mockk<SearchRepository>()
-    val reposRepoMock = mockk<ReposRepository>()
+    private val searchRepoMock = mockk<SearchRepository>()
+    private val reposRepoMock = mockk<ReposRepository>()
 
 
 
@@ -49,5 +49,22 @@ class RepoSearchViewModelTest {
         searchViewModel.search()
         val result = searchViewModel.searchViewData.getOrAwaitValue()
         assertEquals(result.size, 0)
+    }
+
+    @FlowPreview
+    @ExperimentalCoroutinesApi
+    @Test
+    fun getSearchViewData_emit1item() = runBlockingTest {
+        val searchResult = listOf<Repository>( MOCK_REPOS[0])
+
+        coEvery { searchRepoMock.search(any()) } returns searchResult
+        every { reposRepoMock.getRepos() } returns flow {
+            emit(MOCK_REPOS)
+        }
+        val searchViewModel = RepoSearchViewModel(searchRepoMock, reposRepoMock)
+        searchViewModel.setQueryText("foobar")
+        searchViewModel.search()
+        val result = searchViewModel.searchViewData.getOrAwaitValue()
+        assertEquals(result.size, 1)
     }
 }
