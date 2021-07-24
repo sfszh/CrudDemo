a5794ffc46284514d7c7312df46320477b475bef
Rui Zhang <rayruizhang1@gmail.com>
2021-07-24 20:11:20 +0800

    update Dependencies.kt

--- buildSrc/src/main/java/co/ruizhang/buildsrc/Dependencies.kt
+++ buildSrc/src/main/java/co/ruizhang/buildsrc/Dependencies.kt
@@ -1,4 +1,3 @@
-
 package co.ruizhang.buildsrc
 
 object Versions {
@@ -9,9 +8,12 @@ object Libs {
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.0.0-beta05"
 
     object Accompanist {
-        const val version = "0.9.0"
+        const val version = "0.13.0"
         const val coil = "com.google.accompanist:accompanist-coil:$version"
         const val insets = "com.google.accompanist:accompanist-insets:$version"
+        const val placeholder = "com.google.accompanist:accompanist-placeholder-material:$version"
+        const val swipeRefresh = "com.google.accompanist:accompanist-swiperefresh:$version"
+
     }
 
     object Arch {
@@ -20,14 +22,14 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.4.32"
+        private const val version = "1.5.10"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
     }
 
     object Coroutines {
-        private const val version = "1.4.2"
+        private const val version = "1.5.1"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
         const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
@@ -37,6 +39,8 @@ object Libs {
         private const val version = "2.9.0"
         const val retrofit = "com.squareup.retrofit2:retrofit:$version"
         const val gson = "com.squareup.retrofit2:converter-gson:$version"
+        const val mockWebServer = "com.squareup.okhttp3:mockwebserver:4.9.1"
+
     }
 
     object Room {
@@ -61,6 +65,10 @@ object Libs {
         const val mockk = "io.mockk:mockk:1.11.0"
     }
 
+    object Kotlinx {
+        const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.2.1"
+    }
+
     object Hilt {
         private const val version = "2.37"
         private const val miscVersion = "1.0.0-alpha03"
@@ -73,9 +81,14 @@ object Libs {
         const val composeNavigation = "androidx.hilt:hilt-navigation-compose:1.0.0-alpha02"
     }
 
+    object Napier {
+        private const val version = "1.5.0"
+        const val napier = "io.github.aakira:napier:$version"
+    }
+
     object AndroidX {
-        const val coreKtx = "androidx.core:core-ktx:1.6.0-alpha01"
-        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha01"
+        const val coreKtx = "androidx.core:core-ktx:1.6.0"
+        const val navigation = "androidx.navigation:navigation-compose:2.4.0-alpha04"
 
         object Activity {
             const val activityCompose = "androidx.activity:activity-compose:1.3.0-alpha07"
@@ -83,7 +96,7 @@ object Libs {
 
         object Compose {
             const val snapshot = ""
-            const val version = "1.0.0-beta07"
+            const val version = "1.0.0-beta09"
 
             const val animation = "androidx.compose.animation:animation:$version"
             const val foundation = "androidx.compose.foundation:foundation:$version"
@@ -94,16 +107,17 @@ object Libs {
             const val tooling = "androidx.compose.ui:ui-tooling:$version"
             const val ui = "androidx.compose.ui:ui:$version"
             const val uiUtil = "androidx.compose.ui:ui-util:$version"
-            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha05"
+            const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07"
             const val liveData = "androidx.compose.runtime:runtime-livedata:$version"
             const val uiTest = "androidx.compose.ui:ui-test-junit4:$version"
         }
 
         object Lifecycle {
-            private const val version = "2.3.1"
+            private const val version = "2.4.0-alpha02"
             const val runTime = "androidx.lifecycle:lifecycle-runtime:$version"
+            const val runTimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:$version"
             const val liveDataKtx = "androidx.lifecycle:lifecycle-livedata-ktx:$version"
-            const val extensions= "androidx.lifecycle:lifecycle-extensions:2.2.0"
+            const val extensions = "androidx.lifecycle:lifecycle-extensions:2.2.0"
             const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
 
         }
@@ -112,8 +126,9 @@ object Libs {
             const val constraintLayoutCompose =
                 "androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05"
         }
+
         object DataStore {
-            private const val version =  "1.0.0-beta01"
+            private const val version = "1.0.0-beta01"
             const val preferences = "androidx.datastore:datastore-preferences:$version"
         }
 
